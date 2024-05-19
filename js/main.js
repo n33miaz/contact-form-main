3ee6e31b6c0f5d95d365c345fad2be6eb6329afe
n33miaz <ncormino@gmail.com>
2024-05-19 11:39:01 -0300

    add: func JavaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function enviar() 
+{
+    const inputs = document.querySelectorAll("#primeiro_nome, #ultimo_nome, #email, #solicit_geral, #solicit_suporte, #mensagem, #termos");
+
+    if (inputs.values !== null)
+    {
+
+    }
+    else
+    {
+        alert("Mensagem Enviada!");
+    }
+}
